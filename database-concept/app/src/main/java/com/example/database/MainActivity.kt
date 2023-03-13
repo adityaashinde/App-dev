d549b0d766d6944a925f14e03625bbb81b194dfa
Aditya Shinde <adityashinde4580@gmail.com>
2023-03-14 03:43:23 +0530

    feat : added UI

--- database-concept/app/src/main/java/com/example/database/MainActivity.kt
+++ database-concept/app/src/main/java/com/example/database/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.database
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val signInButton = findViewById<Button>(R.id.btnSignUp)
+        val etName = findViewById<EditText>(R.id.etName)
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val etUniqueId = findViewById<EditText>(R.id.etUniqueId)
+        
     }
 }
