2ba39dde51b4a35cb3e22f4fbc21a7818d0596a9
Aditya Shinde <adityashinde4580@gmail.com>
2023-03-14 10:36:45 +0530

    create Users.kt

--- database-concept/app/src/main/java/com/example/database/MainActivity.kt
+++ database-concept/app/src/main/java/com/example/database/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.database
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewDatabase
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +20,16 @@ class MainActivity : AppCompatActivity() {
         val etEmail = findViewById<EditText>(R.id.etEmail)
         val etPassword = findViewById<EditText>(R.id.etPassword)
         val etUniqueId = findViewById<EditText>(R.id.etUniqueId)
-        
+
+        signInButton.setOnClickListener {
+
+            val name = etName.text
+            val mail = etEmail.text
+            val uniqueId = etUniqueId.text
+            val password = etPassword.text
+
+            database = FirebaseDatabase.getInstance().getReference("Users")
+        }
+
     }
 }
