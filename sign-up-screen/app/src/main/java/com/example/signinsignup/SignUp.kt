8fec4926bf1603b2c85c3c91caf70f39b0723067
Aditya Shinde <adityashinde4580@gmail.com>
2023-03-15 12:17:38 +0530

    Update SignUp.kt

--- sign-up-screen/app/src/main/java/com/example/signinsignup/SignUp.kt
+++ sign-up-screen/app/src/main/java/com/example/signinsignup/SignUp.kt
@@ -2,10 +2,29 @@ package com.example.signinsignup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val etMail = findViewById<TextInputEditText>(R.id.etEmail)
+        val etPassword = findViewById<TextInputEditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+
+            val name = etMail.text.toString()
+            val mail = etPassword.text.toString()
+
+            etMail.text?.clear()
+            etPassword.text?.clear()
+
+            Toast.makeText(applicationContext, "User Registered", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
