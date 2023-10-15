2001fc9af2cfb2996c1211fabbbde6cbb54bba31
Aditya Shinde <adityashinde4580@gmail.com>
2023-10-16 01:28:38 +0530

    face-detection-in-androidApps

--- face-detection-using-ml/app/src/main/java/com/example/facedetection/MainActivity.kt
+++ face-detection-using-ml/app/src/main/java/com/example/facedetection/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.facedetection
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +17,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.facedetection.ui.theme.FaceDetectionTheme
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +30,77 @@ class MainActivity : ComponentActivity() {
 
         buttonCamera.setOnClickListener {
 
+            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            if (intent.resolveActivity(packageManager) != null){
+
+                startActivityForResult(intent, 123)
+            }else{
+                Toast.makeText(this, "Oop's something went wrong!!!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode==123 && resultCode == RESULT_OK){
+
+            val extras = data?.extras
+            val bitmap = extras?.get("data") as? Bitmap
+
+            if (bitmap != null) {
+                detectFace(bitmap)
+            }
+        }
+    }
+
+    private fun detectFace(bitmap: Bitmap?) {
+
+        // High-accuracy landmark detection and face classification
+        val options = FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+            .build()
+
+        val detector = FaceDetection.getClient(options)
+
+        val image = InputImage.fromBitmap(bitmap!!, 0)
+
+        val result = detector.process(image)
+            .addOnSuccessListener { faces ->
+                // Task completed successfully
+
+                var resultText = " "
+                var i = 1
+                for (face in faces){
+                    resultText = "Face Number : $i" +
+                            "\n Smile : ${face.smilingProbability?.times(100)}%"
+                            "\n Left Eye Open : ${face.leftEyeOpenProbability?.times(100)}%"
+                            "\n Right Eye Open : ${face.rightEyeOpenProbability?.times(100)}%"
+
+                    i++
+                }
+
+                if (faces.isEmpty()){
+                    Toast.makeText(this, "NO FACE DETECTED!!!", Toast.LENGTH_SHORT).show()
+
+                }else{
+                    Toast.makeText(this, resultText, Toast.LENGTH_LONG).show()
+
+                }
+            }
+            .addOnFailureListener { e ->
+                // Task failed with an exception
+                Toast.makeText(this, "Something wrong ", Toast.LENGTH_SHORT).show()
+
+
+            }
+
+    }
+
+
+
+
 }
